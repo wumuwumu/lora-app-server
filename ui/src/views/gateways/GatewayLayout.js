d6fc990d3e4bfd3077a19e67452436a610154872
Orne Brocaar <info@brocaar.com>
2018-08-27 15:58:08 +0200

    Implement multicast-group web-interface.

--- ui/src/views/gateways/GatewayLayout.js
+++ ui/src/views/gateways/GatewayLayout.js
@@ -5,7 +5,6 @@ import { withStyles } from "@material-ui/core/styles";
 import Grid from '@material-ui/core/Grid';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import Badge from '@material-ui/core/Badge';
 
 import Delete from "mdi-material-ui/Delete";
 
